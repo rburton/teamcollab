70092780e7bae3f66472fb54d3735babeb87ceb9
Richard Burton <mrburton@gmail.com>
2025-02-28 15:34:49 -0500

    initial version

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,7 +1,14 @@
+// Import Turbo
+import * as Turbo from "@hotwired/turbo"
+
 // Stimulus setup
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
+// Start Turbo
+Turbo.start()
+
+// Start Stimulus
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
