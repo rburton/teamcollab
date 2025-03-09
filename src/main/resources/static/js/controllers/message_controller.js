206c8a3b31457df4eb349c00b85a73794bb7e99a
Richard Burton <mrburton@gmail.com>
2025-03-08 19:11:59 -0500

    more

--- src/main/resources/static/js/controllers/message_controller.js
+++ src/main/resources/static/js/controllers/message_controller.js
@@ -1,11 +1,13 @@
 import {Controller} from "@hotwired/stimulus"
+import showdown from "showdown"
 
 export default class extends Controller {
     static targets = ["message"]
 
     connect() {
-        this.messageTarget.innerHTML = this.element.dataset.messageContentValue;
+        const converter = new showdown.Converter();
+        const markdown = this.element.dataset.messageContentValue;
+        const html = converter.makeHtml(markdown);
+        this.messageTarget.innerHTML = html;
     }
-
-
 }
